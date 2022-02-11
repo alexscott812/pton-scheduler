a62facc10196b62c7d21c6f67b8eb3c3ca908148
alexscott812 <alexscott@alexs-mbp-2.myfiosgateway.com>
2022-02-11 12:40:58 -0500

    added types

--- src/utils/get-daily-times.ts
+++ src/utils/get-daily-times.ts
@@ -1,7 +1,7 @@
 const getDailyTimes = ( interval: number = 5 ): string[] => {
-  const times = [];
-  const date = new Date();  
-  for (let minutes = 0; minutes < 24 * 60; minutes = minutes + interval) {
+  const times: string[] = [];
+  const date: Date = new Date();  
+  for (let minutes: number = 0; minutes < 24 * 60; minutes = minutes + interval) {
     date.setHours(0);
     date.setMinutes(minutes);
     times.push(date.toLocaleTimeString('en', { hour: 'numeric', minute: 'numeric' }));
